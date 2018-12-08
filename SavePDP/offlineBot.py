07e98a28ff05beb04b07158d8b075c9b1aa36ee6
iftekhar ul haq <ashfaq.28010@gmail.com>
2018-12-09 00:06:06 +0530

    First Commit

--- SavePDP/offlineBot.py
+++ SavePDP/offlineBot.py
@@ -1,13 +1,16 @@
 #the Script
 import urllib.request
 import json
-
-cha=['pewdiepie','tseries']
+p="UC-lHJZR3Gqxm24_Vd_AJ5Yw"
+t="UCq-Fj5jknLsUf-MWSy4_brA"
+sp="UCEgdi0XIXXZ-qJOFPf4JSKw"
+cha=["UC-lHJZR3Gqxm24_Vd_AJ5Yw","UCq-Fj5jknLsUf-MWSy4_brA","UCEgdi0XIXXZ-qJOFPf4JSKw"]
+#cha2=[p,t,sp]
 key="AIzaSyD_wHoIHLe6yyYAbAZL4l6YDy_4iZd1vk8"
 lis=list()
 i=-1
 for cha in cha:
-    data=urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername="+cha+"&key="+key).read()
+    data=urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id="+cha+"&key="+key).read()
     subs=lis.append(json.loads(data)["items"][0]["statistics"]["subscriberCount"])
     
 
@@ -15,14 +18,17 @@ for cha in cha:
 lis=[ int(x) for x in lis]
 
 
-a=lis[0]-lis[1]
-
-if(a>0):
-    print("Pewdiepie is leading with [" +"{:,}".format(a) + "]9 year olds" )
+a1=lis[0]-lis[1]
+a2=lis[2]-lis[1]
+if(a1>0):
+    print("Pewdiepie is leading with " +"{:,}".format(a1) + " nine year olds \n Pewds is "+ str(a2)+" behind YT sports" )
+    #print("Pewds is "+ str(a2) +"behind YT sports" )
 else:
     print("Pewdiepie has lost the battle as T-Series is"+"{:,}".format(a)+"ahead of us")
-while i<0:
-    print("Pewdiepie has "+ "{:,}".format(lis[0]))
-    print("T-Series has " +"{:,}".format(lis[1]))
-    i=1
+
+print("Pewdiepie has "+ "{:,}".format(lis[0]))
+print("T-Series has " +"{:,}".format(lis[1]))
+print("Yt sports " +"{:,}".format(lis[2]))
+    
+   
 

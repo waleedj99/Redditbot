009092050b6da3eee0cc3f3f9331c733bdd7e4b4
iftekhar ul haq <ashfaq.28010@gmail.com>
2018-12-10 22:30:36 +0530

    Yt gaming

--- SavePDP/Bot.py
+++ SavePDP/Bot.py
@@ -37,8 +37,8 @@ def Rate():
     #"Baby has " +"{:,} ".format(ls[1])+"dislikes.")
 #print("Yt sports " +"{:,}".format(lis[2]))
 def check():
-    cha=["UC-lHJZR3Gqxm24_Vd_AJ5Yw","UCq-Fj5jknLsUf-MWSy4_brA","UCEgdi0XIXXZ-qJOFPf4JSKw"]
-    #Namecha=['pewdiepie','tseries','ytsports'] #name of youtube channels
+    cha=["UC-lHJZR3Gqxm24_Vd_AJ5Yw","UCq-Fj5jknLsUf-MWSy4_brA","UCEgdi0XIXXZ-qJOFPf4JSKw","UCOpNcN46UbXVtpKMrmU4Abg"]
+    #Namecha=['pewdiepie','tseries','ytsports','ytgaming'] #name of youtube channels
     key="AIzaSyD_wHoIHLe6yyYAbAZL4l6YDy_4iZd1vk8" #Youtube Api
     lis=list()
     
@@ -50,14 +50,14 @@ def check():
     
 
     a1=lis[0]-lis[1]#differnce between pewds and T series
-    a2=lis[2]-lis[0]# differnce between Yt sports and pewds 
+    a2=lis[2]-lis[0]# differnce between Yt sports and pewds
+    a3=lis[3]-lis[0]
     if(a1>0):
         if(a2>0):
              comment.reply("Pewdiepie is ahead of T-Series by " + "{:,}".format(a1) + " subs and requires only  "+"{:,}".format(a2)+" more subs to beat Youtube Sports!"+
                       "    \n\nYoutube sports:"+" \t\t" + "{:,}".format(lis[2])+' \t '+"    \n\nPewdiepie :\t\t" + "{:,}".format(lis[0])+'\t'+"    \n\nT-Series :\t\t" + "{:,}".format(lis[1])+'\t'+"\n\n Use ! rewind(without the space for some fun)  \n\nI am a bot.\nFor further information please use '! help' (without the space).    PM me for any feedback. ")
         else:
-            a2=lis[0]-lis[2]
-            comment.reply("Pewdiepie is now the king by being ahead of Youtube sports by" + "{:,}".format(a2) + " nine year olds. "+" \nPewdiepie :  " + "{:,}".format(lis[0])+"  \nYoutube sports:  " + "{:,}".format(lis[2])+" \nT-Series :  " + "{:,}".format(lis[1])+"\n\n\ Use \'! help' (without the space) to mention the owners for any feedback ")
+            comment.reply("Pewdiepie has defeated Youtube Sports and needs more" + "{:,}".format(a3) + " to defeat Youtube Gaming "+" \n\nPewdiepie :  " + "{:,}".format(lis[0])+"  \n\nYoutube Gaming:  " + "{:,}".format(lis[3])+" \n\nT-Series :  " + "{:,}".format(lis[1])+"\n\n\ Use \'! help' (without the space) to mention the owners for any feedback ")
         
             
         #print("posted")#for my console for me to keep a count
@@ -83,7 +83,7 @@ reddit=praw.Reddit(client_id="2T256yp2KjjJOQ", #Reddit Api
                    user_agent="PewdStatus bot (by /u/ashfaq_haq)",
                    username="PewdStatus",
                    password="pewdiepiebot")
-subreddit = reddit.subreddit('PewdiepieSubmissions')
+subreddit = reddit.subreddit('PewdiepieSubmissions')  #("pythonforengineers") 
 CommentList= []
 Keyused = []
 with open('file.txt','a+') as file:

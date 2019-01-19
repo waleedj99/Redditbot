7d13200be5fbb7f4e962f19a4785f9bf51dd2d24
waleedj99 <waleedj1699@gmail.com>
2019-01-19 10:33:39 +0530

    ....

--- SavePDP/Bot.py
+++ SavePDP/Bot.py
@@ -86,8 +86,8 @@ def check():
                       "    \n\nYoutube Gaming:"+" \t\t" + "{:,}".format(lis[3])+' \t '+"    \n\nPewdiepie :\t\t" + "{:,}".format(lis[0])+'\t'
                            +"    \n\nT-Series :\t\t" + "{:,}".format(lis[1])+'\t'+
                            "\n\n Use ! rewind to know the dislikes of rewind   \n\nI am a bot.\nFor further information please use '! help' (without the space).    PM me for any feedback. ")
-        elif(a3<0):
-            comment.reply("Pewdiepie has defeated Youtube Gaming and Pewdiepie is ahead of T-Series by " + "{: , }".format(a1) + " subs  " + "{: , }".format(a3) +" \n\nPewdiepie :  " + "{: , }".format(lis[0])
+        
+        comment.reply("Pewdiepie has defeated Youtube Gaming and Pewdiepie is ahead of T-Series by " + "{: , }".format(a1) + " subs  " + "{: , }".format(a3) +" \n\nPewdiepie :  " + "{: , }".format(lis[0])
                           +"  \n\nYoutube Gaming:  " + "{:,}".format(lis[3])+" \n\nT-Series :  " + "{:,}".format(lis[1])+
                           "\n\n\n Use \'! help' (without the space) to see more commands .PM me for feedback. ")
             
